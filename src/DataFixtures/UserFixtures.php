9cd047403ac2ca06566a1137980c858700f7470c
Jonah Böther <mail@jbtcd.me>
2021-01-18 22:46:25 +0100

    Fix fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
-    private $passwordEncoder;
+    private UserPasswordEncoderInterface $passwordEncoder;
 
      public function __construct(UserPasswordEncoderInterface $passwordEncoder)
      {
@@ -25,6 +25,8 @@ class UserFixtures extends Fixture
             $user,
             'the_new_password'
         ));
+
+        $manager->persist($user);
 
         $manager->flush();
     }
